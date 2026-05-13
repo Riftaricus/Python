f46df88ef40af157b2fdb5a6c40d34930dc2f6bb
Rift <61116182+Riftaricus@users.noreply.github.com>
2026-05-13 15:27:39 +0200

    Removes redundant multiplication for usage percentages
    Simplifies the calculation of CPU, RAM, and disk usage by removing unnecessary multiplication by 100. The data already represents percentages, making the extra operation redundant.
    
    Enhances code clarity and avoids potential confusion without altering functionality.

--- Monitor/bot.py
+++ Monitor/bot.py
@@ -154,9 +154,9 @@ class Bot:
                     data = entry["data"]
 
                     times.append(datetime.datetime.fromtimestamp(entry["timestamp"]))
-                    cpu.append(data["cpu"]["usage_percent"] * 100)
-                    ram.append(data["ram"]["usage_percent"] * 100)
-                    disk.append(data["disk"]["usage_percent"] * 100)
+                    cpu.append(data["cpu"]["usage_percent"])
+                    ram.append(data["ram"]["usage_percent"])
+                    disk.append(data["disk"]["usage_percent"])
 
                 except Exception:
                     continue
